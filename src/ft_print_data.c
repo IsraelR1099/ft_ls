ba8df95b7d47caab488a0930ceb046756f84cd7b
IsraelR1099 <rifarachi10@gmail.com>
2024-07-09 20:14:38 +0200

    print data function

--- src/ft_print_data.c
+++ src/ft_print_data.c
@@ -6,17 +6,19 @@
 /*   By: irifarac <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 11:53:57 by irifarac          #+#    #+#             */
-/*   Updated: 2024/07/08 12:30:51 by irifarac         ###   ########.fr       */
+/*   Updated: 2024/07/09 18:46:36 by israel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/ft_ls.h"
 
-static void	ft_print_dir(t_fileinfo *tmp, t_flags flags)
+static int	ft_count_entries(t_fileinfo *tmp)
 {
 	DIR				*dirp;
 	struct dirent	*direntp;
+	size_t			count;
 
+	count = 0;
 	dirp = opendir(tmp->name);
 	if (!dirp)
 	{
@@ -25,10 +27,16 @@ static void	ft_print_dir(t_fileinfo *tmp, t_flags flags)
 	}
 	while ((direntp = readdir(dirp)) != NULL)
 	{
-		printf("%s ", direntp->d_name);
+		count++;
 	}
-	closedir(dirp);
+	return (count);
+}
+
+static void	ft_print_dir(t_fileinfo *tmp, t_flags flags, int entries)
+{
 	(void)flags;
+	(void)tmp;
+	(void)entries;
 }
 
 static void	ft_print_files(t_fileinfo *tmp, t_flags flags)
@@ -40,6 +48,7 @@ static void	ft_print_files(t_fileinfo *tmp, t_flags flags)
 void	ft_print_data(t_fileinfo *files, t_flags flags)
 {
 	t_fileinfo	*tmp;
+	int		entries;
 
 	tmp = files;
 	if (flags.long_format == false)
@@ -47,7 +56,10 @@ void	ft_print_data(t_fileinfo *files, t_flags flags)
 		while (tmp)
 		{
 			if (tmp->filetype == ft_dir)
-				ft_print_dir(tmp, flags);
+			{
+				entries = ft_count_entries(tmp);
+				ft_print_dir(tmp, flags, entries);
+			}
 			else
 				ft_print_files(tmp, flags);
 			tmp = tmp->next;
